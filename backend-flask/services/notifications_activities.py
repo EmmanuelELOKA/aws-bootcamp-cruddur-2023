e81502ac485234f4750249ee4b6782ab1c7dd441
Emmanue ELOKA <emmanuel.eloka@outlook.com>
2023-07-05 04:39:15 +0000

    implement dynamo db

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -4,8 +4,8 @@ class NotificationsActivities:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Roy Eloks',
-      'message': 'Glad to be a Cloud Bootcamper!',
+      'handle':  'coco',
+      'message': 'I am white unicorn',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -14,31 +14,13 @@ class NotificationsActivities:
       'replies': [{
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Ashi',
-        'message': 'This post is sweet!',
+        'handle':  'worf',
+        'message': 'this post has no honor!',
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
         'created_at': (now - timedelta(days=2)).isoformat()
       }],
-    },
-    {
-      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-      'handle':  'Dibo',
-      'message': 'I am out of apple juice',
-      'created_at': (now - timedelta(days=7)).isoformat(),
-      'expires_at': (now + timedelta(days=9)).isoformat(),
-      'likes': 0,
-      'replies': []
-    },
-    {
-      'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-      'handle':  'Lu',
-      'message': 'My dear engineer, I am just simple DJ',
-      'created_at': (now - timedelta(hours=1)).isoformat(),
-      'expires_at': (now + timedelta(hours=12)).isoformat(),
-      'likes': 0,
-      'replies': []
     }
     ]
     return results
